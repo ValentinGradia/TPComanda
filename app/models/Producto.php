f5eb4e8a495189394315cfd66941bddaba23e73d
Valentin Gradia <valentingonzalezgradia@gmail.com>
2024-06-05 19:56:18 -0300

    solucionando error merge

--- app/models/Producto.php
+++ app/models/Producto.php
@@ -1,23 +1,24 @@
 <?php
 
-require_once "../app/db/AccesoDatos.php";
-
 class Producto
 {
     public $id_producto;
     public $tipo;
     public $nombre;
-    public $codigo_pedido;
-    //private $precio;
+    public $precio;
+    public $cantidad;
+    public $estado_producto;
 
     public function CrearProducto()
     {
         $objetoAccesoDatos = AccesoDatos::obtenerInstancia();
 
-        $sql = $objetoAccesoDatos->prepararConsulta("INSERT INTO productos(tipo,nombre,codigo_pedido) VALUES (:tipo,:nombre,:codigo_pedido)");
+        $sql = $objetoAccesoDatos->prepararConsulta("INSERT INTO productos(tipo,nombre,precio,cantidad,estado_producto) VALUES (:nombre,:nombre,:precio,:cantidad,:estado_producto)");
         $sql->bindValue(":tipo", $this->tipo, PDO::PARAM_STR);
         $sql->bindValue(":nombre", $this->nombre, PDO::PARAM_STR);
-        $sql->bindValue(":codigo_pedido", $this->codigo_pedido, PDO::PARAM_INT);
+        $sql->bindValue(":precio", $this->precio);
+        $sql->bindValue(":cantidad", $this->cantidad);
+        $sql->bindValue(":estado_producto", $this->estado_producto);
 
         $sql->execute();
     }
@@ -25,7 +26,7 @@ class Producto
     public static function ObtenerTodos()
     {
         $objAccesoDatos = AccesoDatos::obtenerInstancia();
-        $consulta = $objAccesoDatos->prepararConsulta("SELECT id_producto,tipo,nombre,codigo_pedido FROM productos");
+        $consulta = $objAccesoDatos->prepararConsulta("SELECT id_producto,tipo,nombre,precio,cantidad,estado_producto FROM productos");
         $consulta->execute();
 
         return $consulta->fetchAll(PDO::FETCH_CLASS, 'Producto');
@@ -35,32 +36,34 @@ class Producto
     public static function ObtenerProducto($id_producto)
     {
         $objAccesoDatos = AccesoDatos::obtenerInstancia();
-        $consulta = $objAccesoDatos->prepararConsulta("SELECT id_producto,tipo,nombre,codigo_pedido FROM productos WHERE id_producto = :id_producto");
+        $consulta = $objAccesoDatos->prepararConsulta("SELECT id_producto,tipo,nombre,precio,cantidad,estado_producto FROM productos WHERE id_producto = :id_producto");
         $consulta->bindValue(':id_producto', $id_producto, PDO::PARAM_STR);
         $consulta->execute();
 
         return $consulta->fetchObject('Producto');
     }
 
-    // public static function modificarProducto($producto)
-    // {
-    //     $objAccesoDato = AccesoDatos::obtenerInstancia();
-    //     $consulta = $objAccesoDato->prepararConsulta("UPDATE productos SET tipo = :tipo, nombre=:nombre, codigo_pedido=:codigo_pedido WHERE id_producto = :id_producto");
-    //     $consulta->bindValue(":codigo_pedido", $producto->codigo_pedido);
-    //     $consulta->bindValue(":tipo", $producto->tipo);
-    //     $consulta->bindValue(":nombre", $producto->nombre);
-    //     $consulta->bindValue(":id_producto", $producto->id_producto);
+    public static function modificarProducto($producto)
+    {
+        $objAccesoDato = AccesoDatos::obtenerInstancia();
+        $consulta = $objAccesoDato->prepararConsulta("UPDATE productos SET tipo = :tipo, nombre=:nombre, precio=:precio, cantidad=:cantidad,
+        estado_producto=:estado_producto WHERE id_producto = :id_producto");
+        $consulta->bindValue(":tipo", $producto->tipo);
+        $consulta->bindValue(":nombre", $producto->nombre);
+        $consulta->bindValue(":precio", $producto->precio);
+        $consulta->bindValue(":cantidad", $producto->cantidad);
+        $consulta->bindValue(":estado_producto", $producto->estado_producto);
+        $consulta->bindValue(":id_producto", $producto->id_producto);
 
-    //     $consulta->execute();
-    // }
+        $consulta->execute();
+    }
 
-    // public static function borrarUsuario($usuario)
-    // {
-    //     $objAccesoDato = AccesoDatos::obtenerInstancia();
-    //     $consulta = $objAccesoDato->prepararConsulta("UPDATE usuarios SET fechaBaja = :fechaBaja WHERE id = :id");
-    //     $fecha = new DateTime(date("d-m-Y"));
-    //     $consulta->bindValue(':id', $usuario, PDO::PARAM_INT);
-    //     $consulta->bindValue(':fechaBaja', date_format($fecha, 'Y-m-d H:i:s'));
-    //     $consulta->execute();
-    // }
+    public static function borrarProducto($producto)
+    {
+        $objAccesoDato = AccesoDatos::obtenerInstancia();
+        $consulta = $objAccesoDato->prepararConsulta("DELETE FROM productos WHERE id_producto = :id");
+        $consulta->bindValue(":id",$producto->id_producto, PDO::PARAM_INT);
+        $consulta->execute();
+
+    }
 }
